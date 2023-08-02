25e60e25aa4bc59bc823202c5125261a4212296e
Sumesh <sumeshsidharth@outlook.com>
2023-08-02 14:26:01 -0500

    first commit

---
+++ app-api/Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> GetCourseDetails()
+        {
+            return new string[] { "Aws", "Azure" };
+        }
+
+        
+    }
+}
